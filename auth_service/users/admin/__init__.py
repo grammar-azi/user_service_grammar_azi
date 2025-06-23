6654bb9298124e3d50d0057dccf4a592c8d44d8a
https://github.com/Merhemet-Huseynov <merhemethuseynov795@gmail.com>
2025-06-24 03:00:00 +0400

    change

--- auth_service/users/admin/__init__.py
+++ auth_service/users/admin/__init__.py
@@ -1,4 +1,4 @@
 from .daily_message_limit import DailyMessageLimitAdmin
 from .daily_messages import DailyMessageAdmin
 from .email_verification import VerificationCodeAdmin
-from .user import CustomUser
+from .user import CustomUserAdmin
