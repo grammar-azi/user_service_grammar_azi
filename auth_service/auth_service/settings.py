3e08ad4c6ce5d2ad2dd48324e850b7782cc3534b
https://github.com/Merhemet-Huseynov <merhemethuseynov795@gmail.com>
2025-06-25 00:14:07 +0400

    Auto run migrate and collectstatic in Render

--- auth_service/auth_service/settings.py
+++ auth_service/auth_service/settings.py
@@ -28,11 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "True") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://user-service-grammar-azi.onrender.com",
+]
 
 # Application definition
 
