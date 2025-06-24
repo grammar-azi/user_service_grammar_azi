61acf34c49f2888bd1d6e8f1b57f57e3b1bae14a
https://github.com/Merhemet-Huseynov <merhemethuseynov795@gmail.com>
2025-06-24 23:56:59 +0400

    Auto run migrate and collectstatic in Render

--- create_superuser.py
+++ create_superuser.py
@@ -7,12 +7,11 @@ django.setup()
 
 User = get_user_model()
 
-username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
 email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
 password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminpass')
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print(f"Superuser '{username}' created.")
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password)
+    print(f"Superuser with email '{email}' created.")
 else:
-    print(f"Superuser '{username}' already exists.")
+    print(f"Superuser with email '{email}' already exists.")
